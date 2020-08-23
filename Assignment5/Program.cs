7aa0301be7e2fdc64f53a0488d231e68be87e01e
Arshdeep56 <arshdeepsingh5656@gmail.com>
2020-08-23 19:23:36 -0400

    Add project files.

---
+++ Assignment5/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+// Name = Arshdeep Singh
+// Student id no =  301118326
+// Date = 18/08/2020
+// Modified Date = 21/08/2020
+// Assignment 5 
+namespace Assignment5
+{
+    static class Program
+    {
+        public static SplashForm splashForm;
+        public static StartForm startForm;
+        public static SelectForm selectForm;
+        public static Product selectedProduct;
+        public static ProductInfoForm productInfoForm;
+        public static OrderForm orderForm;
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            splashForm = new SplashForm();
+            startForm = new StartForm();
+            selectForm = new SelectForm();
+            productInfoForm = new ProductInfoForm();
+            orderForm = new OrderForm();
+
+            Application.Run(splashForm);
+        }
+    }
+}
